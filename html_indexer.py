b42e3aee68b421b35d98e48e4a44e71a255a7287
Bogdan Carpusor <bogdan.carpusor@gmail.com>
2016-03-28 19:02:09 +0300

    added path dictionary method

--- html_indexer.py
+++ html_indexer.py
@@ -6,6 +6,7 @@ import os
 from timeit import default_timer as timer
 import multiprocessing
 from word_counter import WordCounter
+import mmh3
 
 ROOT_FOLDER = 'var'
 WORKERS = 4
@@ -13,15 +14,34 @@ STOP_WORDS = ['a','about','above','after','again','against','all','am','an','and
 SPECIAL_WORDS = ['TOM']
 
 
+def path_hash(root_dir, extension):
+    file_list = get_file_list(root_dir, extension)
+    hash_list = []
+    for file_path in file_list:
+        file_name = os.path.split(file_path)[1]
+        path_name = os.path.split(file_path)[0]
+        hash_list.append("".join([
+                         str(mmh3.hash(file_name)),
+                         ' ',
+                         file_name,
+                         ' ',
+                         path_name]
+        ))
+    return hash_list
+
+def create_dictionary():
+    pass
+
+
 def get_file_list(rootdir, extension):
-    html_list = []
+    file_list = []
     for root, subfolders, files in os.walk(rootdir):
         for input_file in files:
             filename_array = os.path.splitext(input_file)
             if filename_array[-1] in extension:
                 input_file_path = os.path.join(root, input_file)
-                html_list.append(input_file_path)
-    return html_list
+                file_list.append(input_file_path)
+    return file_list
 
 
 def parse_html(input_file_path):
@@ -44,16 +64,18 @@ def count_words(input_file_path):
         print("Can not open file %s", input_file_path)
 
 if __name__ == "__main__":
+    var = path_hash(ROOT_FOLDER, ['.html'])
     # start = timer()
     # result = get_file_list(ROOT_FOLDER, ['.html', '.htm'])
     # multiprocessing.Pool(WORKERS).map(parse_html, result, chunksize=1)
     # end = timer()
     # print(end-start)
-
-    start = timer()
-    result = get_file_list(ROOT_FOLDER, ['.content'])
-    # for file_name in result:
-    #     count_words(file_name)
-    multiprocessing.Pool(WORKERS).map(count_words, result, chunksize=1)
-    end = timer()
-    print(end-start)
+    #
+    # start = timer()
+    # result = get_file_list(ROOT_FOLDER, ['.content'])
+    # # for file_name in result:
+    # #     count_words(file_name)
+    # multiprocessing.Pool(WORKERS).map(count_words, result, chunksize=1)
+    # end = timer()
+    # print(end-start)
+    print('something')
